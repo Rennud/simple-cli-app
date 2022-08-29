66e7defc01b2a0d8bc4f754d066f5def847a3887
MartinS <martin.sedlak777@gmail.com>
2022-08-29 11:29:05 +0200

    Changed manager name to be more explicit

--- src/file_client/commands_manager/manager.py
+++ src/file_client/commands_manager/manager.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 
-def manager(response, output):
+def file_client_manager(response, output):
     '''Manager for handling reponses from BE'''
     data = response.json()
     if output == '-':
